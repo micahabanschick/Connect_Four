4e62e27e6a00e56fcd25ed1ccab2a56b4dcf8418
micahabanschick <micha.ban@gmail.com>
2021-06-15 19:23:01 -0400

    loop and call checkBoard()

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert("Works")
-    
+document.addEventListener('DOMContentLoaded', () => {    
     const squares = document.querySelectorAll('.grid div')
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#current-player')
@@ -105,6 +103,26 @@ document.addEventListener('DOMContentLoaded', () => {
           {
             result.innerHTML = 'Player Two Wins!'
           }
+        }
+      }
+
+      for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+          //if the square below your current square is taken, you can go ontop of it
+          if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-one')
+              currentPlayer = 2
+              displayCurrentPlayer.innerHTML = currentPlayer
+            } else if (currentPlayer == 2){
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-two')
+              currentPlayer = 1
+              displayCurrentPlayer.innerHTML = currentPlayer        
+            } 
+          } else alert('cant go here')
+          checkBoard()
         }
       }
   })
